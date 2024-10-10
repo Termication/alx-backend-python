5736dd38879f5c0545f2212452e1cc2e334ec27c
Innocent <terminalkarabo@gmail.com>
2024-10-10 19:33:55 +0000

    a type-annotated function floor which takes a float n as argument and returns the floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Module for performing mathematical operations.
+'''
+
+def floor(a: float) -> int:
+    '''Calculates the floor value of a floating-point number.
+
+    Args:
+        a (float): The number to be floored.
+
+    Returns:
+        int: The largest integer less than or equal to the given number.
+    '''
+    return int(a)
