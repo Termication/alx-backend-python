b90f42360506c2a4be0f91d7543e6f1cc04c842c
Innocent <terminalkarabo@gmail.com>
2024-11-04 15:54:14 +0000

    implement the TestGetJson.test_get_json method to test that utils.get_json returns the expected result

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,27 +2,32 @@
 """A module for testing the utils module.
 """
 import unittest
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple
 from parameterized import parameterized
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
-    
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(
-            self,
-            nested_map: Dict,
-            path: Tuple[str],
-            expected: Union[Dict, int],
-            ) -> None:
+    def test_access_nested_map(self, nested_map: Dict, path: Tuple[str], expected: int) -> None:
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict, path: Tuple[str], expected_message: str) -> None:
+        """Tests `access_nested_map` raises KeyError with the correct message."""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(e.exception), expected_message)
 
 
 if __name__ == '__main__':
